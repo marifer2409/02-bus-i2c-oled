f615a332e0bb55fb69e6ac7931f136832356b407
marifer2409 <mariafernandaarmijos3@gmail.com>
2026-09-21 05:07:33 +0000

    fett(03_bloque03)completado bloque3

--- bloque_3/src/bloque_3.ino
+++ bloque_3/src/bloque_3.ino
@@ -19,25 +19,24 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET_PIN);
 // Función modular reutilizable para imprimir módulos con estado alineado
 void logBoot(const char* moduleName, bool isOk) {
     // TODO 3.1: Imprimir el nombre del módulo en la posición actual del cursor:
-    // display.print(moduleName);
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+     display.print(moduleName);
+
 
     // TODO 3.2: Alinear el estado a la derecha en la columna X=95 sin alterar la fila Y actual.
     // Pregunta Guía: ¿Por qué usamos display.getCursorY() en lugar de un número fijo como 20?
-    // Pista: display.setCursor(95, display.getCursorY());
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+    display.setCursor(95, display.getCursorY());
+
 
     // TODO 3.3: Según el valor de isOk (true/false), imprimir "[OK]" o "[ERR]":
-    // if (isOk) {
-    //     display.println("[OK]");
-    // } else {
-    //     display.println("[ERR]");
-    // }
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+ if (isOk) {
+     display.println("[OK]");
+     } else {
+         display.println("[ERR]");
+     }
 
     // TODO 3.4: Volcar los cambios del buffer al vidrio físico y pausar 200ms para efecto visual:
-    // display.display();
-    // delay(200);
+     display.display();
+     delay(200);
 }
 
 void setup() {
@@ -64,9 +63,8 @@ void setup() {
     display.display();
 
     // TODO 3.5: Probar la función logBoot() con dos módulos de prueba:
-    // logBoot("ESP32 Core", true);
-    // logBoot("Sensor I2C", true);
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+     logBoot("ESP32 Core", true);
+     logBoot("Sensor I2C", true);
 
     Serial.println("[BLOQUE 3] Telemetría renderizada correctamente.");
 }
