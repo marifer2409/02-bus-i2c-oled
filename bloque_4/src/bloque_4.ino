aa3842f4f971ee3f44c725f61ae81bf784ff6d78
marifer2409 <mariafernandaarmijos3@gmail.com>
2026-09-22 03:55:44 +0000

    fett(04_bloque4)completada

--- bloque_4/src/bloque_4.ino
+++ bloque_4/src/bloque_4.ino
@@ -91,20 +91,20 @@ void runSystemPOST() {
     Serial.println("   ESP32: POWER-ON SELF TEST (POST)       ");
     Serial.println("==========================================");
 
-    // TODO 4.1: Mostrar la cabecera visual invocando showBootHeader()
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+    // TODO 4.1: Mostrar la cabecera visual invocando 
+    showBootHeader()
+    
 
     // TODO 4.2: Probar secuencialmente los 4 subsistemas usando logBoot(nombre, estado):
-    // 1. logBoot("ESP32 240MHz", true);
-    // 2. logBoot("I2C @ 400kHz", true);
-    // 3. logBoot("OLED 0x3C", true);
-    // 4. logBoot("Bateria 8.4V", true);
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+     logBoot("ESP32 240MHz", true);
+     logBoot("I2C @ 400kHz", true);
+     logBoot("OLED 0x3C", true);
+     logBoot("Bateria 8.4V", true);
 
     delay(500);
 
-    // TODO 4.3: Concluir la rutina mostrando la barra final con showSystemReady()
-    /* ESCRIBE TU CÓDIGO AQUÍ */
+    // TODO 4.3: Concluir la rutina mostrando la barra final con 
+    showSystemReady()
 }
 
 void setup() {
@@ -122,6 +122,11 @@ void setup() {
     //    Ejecutar runSystemPOST();
     // 3. Si no, reportar falla por el Serial Monitor.
     /* ESCRIBE TU CÓDIGO AQUÍ */
+    int total = scanI2CBus();
+    if (total > 0 && initDisplay()){
+    }else{
+    serial.println("[BOOT] ERROR: No se pudo completar el diagnóstico del sistema.");
+    }
 }
 
 void loop() {
